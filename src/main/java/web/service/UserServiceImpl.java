79f9e5686fe9852fa9cefc12086859277289baf7
96test96 <97890676+96test96@users.noreply.github.com>
2023-03-12 22:31:49 +0300

    transactional1

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import web.entity.User;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
@@ -24,13 +24,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
@@ -39,6 +37,5 @@ public class UserServiceImpl implements UserService {
     public User getUserById(int id) { return userDao.getUserById(id); }
 
     @Override
-    @Transactional
     public void deleteUser(int id) { userDao.deleteUser(id); }
 }
